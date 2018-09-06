236ba7ef029f1a1961a6da404526c23f1dc4822a
Eduardo de la Cruz Ibarra <eduardoibarra904@gmail.com>
2018-09-06 10:24:37 -0500

    changes on fallidas

--- mobile/src/pages/fallidas/fallida/fallida.ts
+++ mobile/src/pages/fallidas/fallida/fallida.ts
@@ -1,11 +1,12 @@
 import {Component, ViewChild} from '@angular/core';
-import {IonicPage, NavController, NavParams, ToastController} from 'ionic-angular';
+import {IonicPage, LoadingController, NavController, NavParams, ToastController} from 'ionic-angular';
 import {SignaturePad} from "angular2-signaturepad/signature-pad";
 import {MedidorProvider} from "../../../providers/medidor/medidor";
 import {FallidaProvider} from "../../../providers/fallida/fallida";
 import {FallidasPage} from "../fallidas";
 import {AuthenticationProvider} from "../../../providers/authentication/authentication";
 import {UserProvider} from "../../../providers/user/user";
+import {Storage} from "@ionic/storage";
 
 @IonicPage()
 @Component({
@@ -23,12 +24,16 @@ export class FallidaPage {
   today: any = Date.now();
   user: any;
   showing = false;
+  offline_fallidas:any [] = [];
+  isOffline = false;
   constructor(public navCtrl: NavController, public navParams: NavParams,
               private medidorProvider: MedidorProvider,
               private toastController: ToastController,
               public fallidaProvider: FallidaProvider,
               public authenticationProvider: AuthenticationProvider,
-              public userProvider: UserProvider) {
+              public userProvider: UserProvider,
+              private loadingCtrl: LoadingController,
+              private storage: Storage) {
     this.authenticationProvider.getStatus().subscribe((data) => {
       this.userProvider.getById(data.uid).valueChanges().subscribe((data2) => {
         this.user = data2;
@@ -43,8 +48,17 @@ export class FallidaPage {
       console.log(this.fallida);
       this.showing = true;
     }
+    if (this.navParams.get('offline')) {
+      this.isOffline = this.navParams.get('offline');
+    }
   }
-
+  ionViewWillEnter() {
+    this.storage.get('offline_fallidas').then((data) => {
+      this.offline_fallidas = JSON.parse(data || '[]');
+    }).catch((error) => {
+      console.log(error);
+    });
+  }
   ionViewDidLoad() {
     console.log('ionViewDidLoad FallidaPage');
     window.setTimeout(() => {
@@ -90,16 +104,49 @@ export class FallidaPage {
     this.signaturePad.set('canvasHeight', canvas.offsetHeight);
   }
   save() {
-    this.fallida.timestamp = Date.now();
+    this.fallida.timestamp = (this.fallida.timestamp) ? this.fallida.timestamp : Date.now();
     this.fallidaProvider.add(this.fallida, this.user).then((data) => {
       const toast = this.toastController.create({
           message: 'Información enviada con éxito', duration: 4000, position: 'bottom'
         });
       toast.present();
+      if (this.isOffline) {
+        if (this.offline_fallidas) {
+          this.offline_fallidas.forEach((of, i) => {
+            if (of.timestamp == this.fallida.timestamp) {
+              this.offline_fallidas.splice(i, 1);
+              this.storage.set('offline_fallidas', JSON.stringify(this.offline_fallidas)).then((data) => {
+              }).catch((error) => {
+                console.log(error);
+              });
+            }
+          });
+        }
+      }
       this.fallida = {};
       this.navCtrl.setRoot(FallidasPage);
     }).catch((error) => {
       alert('Ocurrió un error al enviar la información');
+      console.log(error);
+    });
+  }
+  finishOffline() {
+    let loading = this.loadingCtrl.create({
+      content: 'Por favor espera mientras se envía el formulario...'
+    });
+    loading.present();
+    this.fallida.timestamp = Date.now();
+    this.fallida.guardado_offline = true;
+    this.offline_fallidas.push(this.fallida);
+    this.storage.set('offline_fallidas', JSON.stringify(this.offline_fallidas)).then((data) => {
+      loading.dismiss();
+      const toast = this.toastController.create({message: '¡Visita fallida guardada localmente con éxito!', duration: 5000, position: 'bottom'});
+      toast.present();
+      this.fallida = {};
+      this.navCtrl.setRoot(FallidasPage);
+    }).catch((error) => {
+      alert('Ocurrió un error mientras se guardaba la visita fallida:  ' + JSON.stringify(error));
+      loading.dismiss();
       console.log(error);
     });
   }
--- mobile/src/pages/fallidas/fallidas.ts
+++ mobile/src/pages/fallidas/fallidas.ts
@@ -5,6 +5,7 @@ import {FallidaProvider} from "../../providers/fallida/fallida";
 import {AuthenticationProvider} from "../../providers/authentication/authentication";
 import {UserProvider} from "../../providers/user/user";
 import {VisitsPage} from "../visits/visits";
+import {Storage} from "@ionic/storage";
 
 @IonicPage()
 @Component({
@@ -13,11 +14,13 @@ import {VisitsPage} from "../visits/visits";
 })
 export class FallidasPage {
   user: any;
+  offline_fallidas: any [] = [];
   constructor(public navCtrl: NavController,
               public navParams: NavParams,
               public fallidaProvider: FallidaProvider,
               private authenticationProvider: AuthenticationProvider,
-              private userProvider: UserProvider) {
+              private userProvider: UserProvider,
+              private storage: Storage) {
     this.authenticationProvider.getStatus().subscribe((data) => {
       if (!data) {
         return;
@@ -37,6 +40,12 @@ export class FallidasPage {
     }, (error) => {
       console.log(error);
     });
+    this.storage.get('offline_fallidas').then((data) => {
+      this.offline_fallidas = JSON.parse(data || '[]');
+      console.log(this.offline_fallidas);
+    }).catch((error) => {
+      console.log(error);
+    });
   }
 
   ionViewDidLoad() {
@@ -47,5 +56,8 @@ export class FallidasPage {
   }
   openVisits(f) {
     this.navCtrl.push(VisitsPage, {form: f});
+  }
+  openFallidaOffline(f) {
+    this.navCtrl.push(FallidaPage, {visit: f, offline: true});
   }
 }
