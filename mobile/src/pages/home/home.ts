5f9a166dd03f6aa061fb325968739ea11c863f4b
Eduardo de la Cruz Ibarra <eduardoibarra904@gmail.com>
2018-09-30 16:17:13 -0500

    latest changes on delete offline forms

--- mobile/src/pages/home/home.ts
+++ mobile/src/pages/home/home.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import {NavController, ToastController} from 'ionic-angular';
 import {FormInstallationPage} from "../form-installation/form-installation";
 import {FormProvider} from "../../providers/form/form";
 import {AuthenticationProvider} from "../../providers/authentication/authentication";
@@ -14,7 +14,8 @@ export class HomePage {
   forms: any[] = [];
   user: any;
   offline_forms: any[];
-  constructor(public navCtrl: NavController, private formProvider: FormProvider, private authenticationProvider: AuthenticationProvider, private userProvider: UserProvider, private storage: Storage) {
+  constructor(public navCtrl: NavController, private formProvider: FormProvider, private authenticationProvider: AuthenticationProvider, private userProvider: UserProvider, private storage: Storage,
+              public toastController: ToastController) {
     this.authenticationProvider.getStatus().subscribe((data) => {
       if (!data) {
         return;
@@ -41,6 +42,22 @@ export class HomePage {
   ionViewWillEnter() {
     this.storage.get('offline_forms').then((data) => {
       this.offline_forms = JSON.parse(data);
+    }).catch((error) => {
+      console.log(error);
+    });
+  }
+  delete(f) {
+    if (!confirm('Seguro que deseas eliminar este formulario? Ya no podrá ser enviado.')) {
+      return;
+    }
+    this.offline_forms.forEach((of, i) => {
+      if (of.uid == f.uid) {
+        this.offline_forms.splice(i, 1);
+      }
+    });
+    this.storage.set('offline_forms', JSON.stringify(this.offline_forms)).then((data) => {
+      const toast = this.toastController.create({message: '¡Formulario eliminado con éxito!', duration: 4000, position: 'bottom'});
+      toast.present();
     }).catch((error) => {
       console.log(error);
     });
