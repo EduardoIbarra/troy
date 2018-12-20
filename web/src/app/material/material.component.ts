ef429532665f528997fb4593bb77d6cb5e0bda3a
Miguel Hernandez <miguelhdzch@gmail.com>
2018-12-20 17:57:47 -0600

    add all totals

--- web/src/app/material/material.component.ts
+++ web/src/app/material/material.component.ts
@@ -47,7 +47,7 @@ export class MaterialComponent implements OnInit {
 
   ngOnInit() {
     this.materialService.totals().valueChanges().subscribe((data) => {
-      console.log('Materiales',data);
+      console.log('Materiales', data);
       this.materials = data;
       this.getSubcontratistas();
     })
@@ -58,11 +58,11 @@ export class MaterialComponent implements OnInit {
       this.subcontratistas = data;
       this.subcontratistas.forEach((s) => {
         this.materialService.totalBySubcontratista(s.id).valueChanges().subscribe((totals) => {
-          s.instalaciones = totals;
+          s.materiales = totals;
         });
       });
 
-      console.log('Subcontratista',this.subcontratistas);
+      console.log('Subcontratista', this.subcontratistas);
       this.spinnerService.hide();
       subscription.unsubscribe();
     }, (error) => {
@@ -71,9 +71,9 @@ export class MaterialComponent implements OnInit {
   }
 
   subcontratistaChanged(value) {
-    console.log(value);
     if (this.selectedSupervisor === 'all') {
       this.materialService.totals().valueChanges().subscribe((data) => {
+        console.log('Materiales', data);
         this.materials = data;
       })
     } else {
@@ -81,5 +81,91 @@ export class MaterialComponent implements OnInit {
         this.materials = totals;
       });
     }
+  }
+
+
+  setTotalAllSubcontratistas() {
+    const formService = this.formService.get().valueChanges().subscribe((forms: any) => {
+
+      let totales: any[] = [];
+      let materiales: any[] = [];
+
+      forms.map((form: any) => {
+        if (!form || !form.current_materials) return;
+
+        let subId;
+        if (form.instalo) {
+          subId = form.instalo.id;
+        } else if (form.user && form.user.company) {
+          subId = form.user.company.id;
+        }
+
+        if (!subId) return;
+
+        if (!totales[subId]) {
+          totales[subId] = []
+        }
+
+
+        form.current_materials.map((mat) => {
+          // mat = {id, current_quantity, unidad, descripcion}
+          if (mat.current_quantity === "" || mat.current_quantity === undefined) return;
+
+          if (!materiales[subId]) {
+            materiales[subId] = [];
+          }
+          materiales[subId].push(mat);
+
+          materiales[subId].map((m, i) => {
+            if (m.id === mat.id) {
+              if (!totales[subId][m.id]) {
+                totales[subId][m.id] = {
+                  id: m.id,
+                  total: parseFloat(m.current_quantity),
+                  unidad: m.unidad,
+                  descripcion: m.descripcion,
+                  sub: subId
+                }
+              } else {
+                totales[subId][m.id].total += parseFloat(m.current_quantity)
+              }
+            }
+            materiales[subId] = materiales[subId].slice(i, 1);
+          });
+        })
+
+      });
+
+
+      totales.map((sub) => {
+        sub.map((mat) => {
+          if (mat.total === "" || mat.total === undefined) return;
+
+          const totalService = this.materialService.totalBySubcontratistaMat(mat.sub, mat.id).valueChanges().subscribe((snap: any) => {
+            let newValue = {
+              id: mat.id,
+              total: parseFloat(mat.total),
+              unidad: mat.unidad,
+              descripcion: mat.descripcion
+            };
+
+            if (snap) {
+              newValue.total += parseFloat(snap.total);
+            }
+
+            this.materialService.settotalBySubcontratistaMat(mat.sub, mat.id, newValue);
+            totalService.unsubscribe();
+          })
+        })
+      });
+
+
+      console.log('Totales', totales);
+      console.log('Total formularios', forms.length);
+
+      this.spinnerService.hide();
+      formService.unsubscribe()
+
+    });
   }
 }
